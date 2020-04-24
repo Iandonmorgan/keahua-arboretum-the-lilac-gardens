b2b15a204e1b842fbae3eab66f9eedd88cbdaac8
Landon Morgan <morganlandon@gmail.com>
2020-04-24 13:26:18 -0500

    Ulae Ulae, Olay olay

--- animals/ulae.py
+++ animals/ulae.py
@@ -10,19 +10,11 @@ class Ulae(Animal, Identifiable, ISwimming, ISaltwater):
         ISwimming.__init__(self)
         ISaltwater.__init__(self)
         self.__prey = {"Fish"}
-        self.hospitable_locations = list()
 
 
     @property
     def prey(self):
         return self.__prey
 
-    def feed(self, prey):
-        if prey in self.__prey:
-            print(f'The Ulae ate {prey} for a meal')
-        else:
-            print(f'The Ulae rejects the {prey}')
-
-
     def __str__(self):
-        return f'Ulae {self.id}. is Ulaeing around!'
+        return f'Ulae {str(self.id).split("-")[0]}. is Ulaeing around!'
