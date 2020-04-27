68d29c80ac1c753d22b92549a7fc4c8bdd15fce2
Landon Morgan <morganlandon@gmail.com>
2020-04-27 16:20:41 -0500

    kikakapu spelling

--- animals/kikakapu.py
+++ animals/kikakapu.py
@@ -1,10 +1,10 @@
 from animals import Animal
 from interfaces import ISwimming, ISaltwater
 
-class Kikapu(Animal, ISwimming, ISaltwater):
+class Kikakapu(Animal, ISwimming, ISaltwater):
 
     def __init__(self):
-        Animal.__init__(self, "Kikapu")
+        Animal.__init__(self, "Kīkākapu")
         ISaltwater.__init__(self)
         ISwimming.__init__(self)
         self.__prey = { "fish" }
